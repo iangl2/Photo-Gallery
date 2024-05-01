1c3044785911226b4c35b7baa72b46d41a39d85b
Ian Lee <ianlee0203@gmail.com>
2024-05-01 17:45:34 -0500

    Added descriptions to the photocards

--- script.js
+++ script.js
@@ -10,8 +10,10 @@ function returnPets(url){
 
     for(let i=0;i<12;i++){
         fetch(url).then(res => res.json()).then(function(data){
-            console.log(data.message);
-
+            
+            const breed= () => { const value =  data.message.split("/")[4]; return value.includes("-") ?  value.split("-")[1] + " " +value.split("-")[0] : value };
+            console.log(data.message)
+            console.log(breed());
             const photocard =document.createElement('div');
             photocard.setAttribute('class','photocard');
             const imageContainer = document.createElement('div')
@@ -19,9 +21,10 @@ function returnPets(url){
             const image = document.createElement('img')
             const  imageDescriptionContainer = document.createElement('div')
             imageDescriptionContainer.setAttribute('class','imageDescriptionContainer')
-            const imageDescription = document.createElement('h3')
+            const imageDescription = document.createElement('h4')
             imageDescription.setAttribute('class', 'imageDescription')
 
+            imageDescription.innerHTML="Breed: "+ breed();
             image.src= data.message;
             imageContainer.appendChild(image)
             imageDescriptionContainer.appendChild(imageDescription)
